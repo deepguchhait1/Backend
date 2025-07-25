fc1ef1f6a61b52a6b4a72a97ed667b106e721522
“username” <emailaddr>
2025-07-25 15:07:37 +0530

    Add the frontend linke

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,11 +5,11 @@ import User from "../models/user.model.js";
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token) {
-      return res
-        .status(401)
-        .json({ message: "unauthorized - No token Provided" });
-    }
+    // if (!token) {
+    //   return res
+    //     .status(401)
+    //     .json({ message: "unauthorized - No token Provided" });
+    // }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (!decoded) {
